01a058a32293564540ffeef01066532befdca346
Maël Vincent <mael.vincent.pro@gmail.com>
2019-09-25 11:05:49 +0200

    array live coding

---
+++ javascript-vanilla/array.js
@@ -0,0 +1,6 @@
+let movies = ["Kill Bill", "Volte face", "Ben hur", "Blanche neige", "Le parrain"];
+//let movies = new Array();
+
+for (let isabelle = 0; isabelle < movies.length; isabelle++) {
+    console.log(movies[isabelle]);
+}
